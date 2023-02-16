c2e35fe0272abde7aedc98e4a342e056411ac2d5
isokenendurance <enduranceeriamiatoe@gmail.com>
2023-02-16 05:55:58 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - program tha prints using the printf function
- * Return: 0 (successful)
+ * main - Program tha prints using the printf function
+ * Return: 0 (Successful)
  */
 int main(void)
 {
