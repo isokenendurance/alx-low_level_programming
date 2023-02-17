d12fab29c5e4ad0647dcf8392e6c100796c37b5f
isokenendurance <enduranceeriamiatoe@gmail.com>
2023-02-17 09:13:20 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints alphabets in lowercase
+ * Return: 0 (successful)
+ */
+int main(void)
+{
+	char lc;
+
+	for (lc = 'a'; lc <= 'z'; lc++)
+	{
+		putchar(lc);
+	}
+	putchar('\n');
+return (0);
+}
