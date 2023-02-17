939631603686fcebc6503360fdadd32101b4f1d4
isokenendurance <enduranceeriamiatoe@gmail.com>
2023-02-17 10:44:37 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints digits number
+ * Return: 0 (successful)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n)
+		printf("%d", n);
+	printf("\n");
+return (0);
+}
