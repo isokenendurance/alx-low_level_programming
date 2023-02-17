cc25a1062cfea11b33e7209edd95e9002ddefaed
isokenendurance <enduranceeriamiatoe@gmail.com>
2023-02-17 11:00:55 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints single digits to base 10
+ * Return: 0 (successful)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		putchar(n + '0');
+	putchar('\n');
+return (0);
+}
