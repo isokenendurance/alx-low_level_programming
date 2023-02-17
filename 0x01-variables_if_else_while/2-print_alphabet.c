2e4272cf5d2412edc5586daeb4fd2f92995799e0
isokenendurance <enduranceeriamiatoe@gmail.com>
2023-02-17 08:23:11 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints the alphabets in lowercase
+ * Return: 0 (successful)
+ */
+int main(void)
+{
+	int ch = 'a';
+
+	while (ch <= '2')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar ('\n');
+return (0);
+}
