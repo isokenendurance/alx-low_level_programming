e2df9328049d53eda5a6bc998f109a6c4d2d0360
isokenendurance <enduranceeriamiatoe@gmail.com>
2023-02-21 13:33:04 -0800

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,15 +2,15 @@
 /**
  * print_last_digit - function that print last digit
  * @i: function parameter
- * Return: 0
+ * Return: k
  */
 
 int print_last_digit(int i)
 {
-	int lc = i % 10;
+	int k;
 
-	if (lc < 0)
-		lc *= -1;
-	_putchar(lc + '0');
-	return (0);
+	k = i % 10;
+	if (i < 0)
+		k = -k;
+	return (k);
 }
