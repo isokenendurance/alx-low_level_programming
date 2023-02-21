75829acc99208f3251c83336d3dd22aa5416fd07
isokenendurance <enduranceeriamiatoe@gmail.com>
2023-02-21 12:45:22 -0800

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _abs - checks for absolute value
+ * @t: parameter to be checked
+ * Return: a (successful)
+ */
+int _abs(int t)
+{
+	if (t >= 0)
+		return (t);
+	else
+		return (-t);
+}
