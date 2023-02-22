c2009eca0c5a03d8d11883abf7bc84db7ff3a71c
isokenendurance <enduranceeriamiatoe@gmail.com>
2023-02-22 13:15:26 -0800

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,37 +7,31 @@
 
 int main(void)
 {
-	unsigned long int i;
-	unsigned long int bef = 1;
-	unsigned long int aft = 2;
-	unsigned long int bef1;
-	unsigned long int bef2;
-	unsigned long int aft1;
-	unsigned long int aft2;
+	unsigned long int fr1 = 0, bk1 = 1, fr2 = 0, bk2 = 2;
+	unsigned long int hold1, hold2, hold3;
+	int count;
 
-	printf("%lu", bef);
-
-	for (i = 1; i < 98; i++)
+	printf("%lu, %lu, ", bk1, bk2);
+	for (count = 2; count < 98; count++)
 	{
-		printf(", %lu", aft);
-		aft += bef;
-		bef = aft - bef;
+		if (bk1 + bk2 > LARGEST || fr2 > 0 || fr1 > 0)
+		{
+			hold1 = (bk1 + bk2) / LARGEST;
+			hold2 = (bk1 + bk2) % LARGEST;
+			hold3 = fr1 + fr2 + hold1;
+			fr1 = fr2, fr2 = hold3;
+			bk1 = bk2, bk2 = hold2;
+			printf("%lu%010lu", fr2, bk2);
+		}
+		else
+		{
+			hold2 = bk1 + bk2;
+			bk1 = bk2, bk2 = hold2;
+			printf("%lu", bk2);
+		}
+		if (count != 97)
+			printf(", ");
 	}
-
-		bef1 = (bef / 1);
-		bef2 = (bef % 1);
-		aft1 = (aft / 1);
-		aft2 = (aft % 1);
-
-		for (i = 92; i < 99; ++i)
-	{
-			printf(", %lu", aft1 + (aft2 / 1));
-			printf("%lu", aft2 % 1);
-			aft1 = aft1 + bef1;
-			bef1 = aft1 - bef1;
-			aft2 = aft2 + bef2;
-			bef2 = aft2 - bef2;
-	}
-		printf("\n");
-		return (0);
+	printf("\n");
+	return (0);
 }
