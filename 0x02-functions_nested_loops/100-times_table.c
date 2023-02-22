4baa3597b77d14acf5c7b6d74fbc5d81ec541a4d
isokenendurance <enduranceeriamiatoe@gmail.com>
2023-02-22 06:36:52 -0800

    101-natural.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,21 +2,20 @@
 /**
  * print_times_table - prints time table
  * @n: times table to use
- * Return: void
  */
 
 void print_times_table(int n)
 {
 	int a = 0, rep, b;
 
-	if (n < 0 || n > 15)
+	if (n > 15 || n < 0)
 		return;
 	while (a <= n)
 	{
-		for (b = 0 ; b <= n ; b++)
+		for (b = 0; b <= n; b++)
 		{
 			rep = a * b;
-			if (b == 0)
+			if (b > 99)
 				_putchar('0' + rep);
 			else if (rep < 10)
 			{
@@ -24,7 +23,7 @@ void print_times_table(int n)
 				_putchar(' ');
 				_putchar('0' + rep);
 			}
-			else if (rep < 100)
+			else if (b > 9)
 			{
 				_putchar(' ');
 				_putchar('0' + rep / 10);
