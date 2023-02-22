18ae4d59d8342f5b9a147ae5371d3cbb06b15dca
isokenendurance <enduranceeriamiatoe@gmail.com>
2023-02-22 12:28:21 -0800

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -16,26 +16,28 @@ int main(void)
 	unsigned long int aft2;
 
 	printf("%lu", bef);
+
 	for (i = 1; i < 91; i++)
 	{
 		printf(", %lu", aft);
 		aft += bef;
 		bef = aft - bef;
 	}
+
 		bef1 = (bef / 1);
 		bef2 = (bef % 1);
 		aft1 = (aft / 1);
 		aft2 = (aft % 1);
 
-		for (i = 92; i < 99; i++)
-		{
+		for (i = 92; i < 99; ++i)
+	{
 			printf(", %lu", aft1 + (aft2 / 1));
 			printf("%lu", aft2 % 1);
 			aft1 = aft1 + bef1;
 			bef1 = aft1 - bef1;
 			aft2 = aft2 + bef2;
 			bef2 = aft2 - bef2;
-		}
+	}
 		printf("\n");
 		return (0);
 }
