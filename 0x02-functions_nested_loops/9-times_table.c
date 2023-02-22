40479ad868edcf7defbef6085e6d384e8b3282dc
isokenendurance <enduranceeriamiatoe@gmail.com>
2023-02-21 23:50:21 -0800

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,23 +5,34 @@
 
 void times_table(void)
 {
-	int num;
-	int mult;
-	int prod;
+	int n;
+	int m;
 
-	for (num = 0 ; num <= 9 ; num++)
+	for (n = 0; n <= 9; n++)
 	{
-		_putchar('0');
-		for (mult = 1 ; mult <= 9 ; mult++)
+		for (m = 0; m <= 9; m++)
+		{
+
+		int prod = n * m;
+
+		if (m == 0)
+		{
+			_putchar('0');
+		}
+		else if (prod <= 9)
 		{
 			_putchar(',');
 			_putchar(' ');
-			prod = num + mult;
-			if (prod <= 9)
-				_putchar(' ');
-			else
-				_putchar((prod / 10) + '0');
-			_putchar((prod % 10) + '0');
+			_putchar(' ');
+			_putchar(prod + '0');
+		}
+		else
+		{
+			_putchar(',');
+			_putchar(' ');
+			_putchar(prod / 10 + '0');
+			_putchar(prod % 10 + '0');
+		}
 		}
 		_putchar('\n');
 	}
